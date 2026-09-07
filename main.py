731d940325cfc1839149d33893cc16bfb17fa31f
Dang Thanh Ngo <ngodangthanh020706@gmail.com>
2026-09-07 19:30:22 +1000

    feat: add create application endpoint

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class Applications(BaseModel):
+class Application(BaseModel):
     id : int
     company: str
     role : str
     status : str
+
+applications = []
+
+@app.post("/applications",status_code = 201)
+def create_application (application : Application):
+    applications.append(application)
+    return application
+
+
