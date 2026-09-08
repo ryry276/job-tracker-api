93f7a65d78af5817a46898ac326bbabd8d437a7b
Dang Thanh Ngo <ngodangthanh020706@gmail.com>
2026-09-08 13:34:54 +1000

    feat: add application filters by status and company

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 app = FastAPI()
 
 class Application(BaseModel):
-    id : int
+    id : int = Field(ge = 0)
     company: str
     role : str
     status : str
@@ -23,11 +23,28 @@ def create_application (application : Application):
     return application
 
 @app.get("/applications")
-def get_all_application():
-    return applications
+def get_applications(status : str | None = None, company : str | None = None ):
+    result = applications
+    if status is not None:
+        filtered = []
+        for application in result:
+            if application.status == status:
+                filtered.append(application)
+        result = filtered
+
+    if company is not None:
+        filtered = []
+        for application in result:
+            if application.company == company:
+                filtered.append(application)
+        result = filtered
+        
+    return result
+
+   
 
 @app.get("/applications/{application_id}")
-def get_application(application_id : int):
+def get_application_by_id(application_id : int):
     for application in applications:
         if application_id == application.id:
             return application
@@ -62,4 +79,4 @@ def delete_application(application_id : int):
         status_code = 404,
         detail= "msg: application not found"
         )
-    
+
