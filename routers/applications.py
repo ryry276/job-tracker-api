918864c92b1be542ed25ce356c1620070735b006
Dang Thanh Ngo <ngodangthanh020706@gmail.com>
2026-09-08 18:58:05 +1000

    refactor: move application routes to router

---
+++ routers/applications.py
@@ -0,0 +1,94 @@
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel, Field
+from typing import Optional, Literal
+
+router=APIRouter(prefix="/applications")
+
+
+ApplicationStatus = Literal["applied", "interview", "rejected"]
+
+class Application(BaseModel):
+    id : int = Field(ge = 0)
+    name : str
+    company: str
+    role : str
+    status : ApplicationStatus 
+
+
+class UpdateApplication(BaseModel):
+    name : Optional[str] = None
+    company : Optional[str] = None
+    role : Optional[str] = None
+    status : ApplicationStatus | None = None
+
+applications = []
+
+@router.post("/applications",status_code = 201)
+def create_application (application : Application):
+    for current_application in applications:
+        if current_application.id == application.id:
+            raise HTTPException(
+                status_code= 409,
+                detail = "application already existed"
+                )
+    applications.append(application)
+    return application
+
+@router.get("/applications")
+def get_applications(status : ApplicationStatus | None = None, company : str | None = None ):
+    result = applications
+    if status is not None:
+        filtered = []
+        for application in result:
+            if application.status == status:
+                filtered.append(application)
+        result = filtered
+
+    if company is not None:
+        filtered = []
+        for application in result:
+            if application.company == company:
+                filtered.append(application)
+        result = filtered
+        
+    return result
+
+   
+
+@router.get("/applications/{application_id}")
+def get_application_by_id(application_id : int):
+    for application in applications:
+        if application_id == application.id:
+            return application
+
+    raise HTTPException(
+        status_code = 404,
+        detail = "msg: application not found"
+    )
+
+@router.patch("/applications/{application_id}")
+def update_application(update : UpdateApplication, application_id : int):
+    for application in applications:
+        if application.id == application_id:
+            data = update.model_dump(exclude_unset=True)
+            for key, value in data.items():
+                setattr(application,key,value)
+            return application
+
+    raise HTTPException(
+            status_code = 404,
+            detail = "msg: application not found"
+        )
+
+@router.delete("/applications/{application_id}", status_code=204)
+def delete_application(application_id : int):
+    for application in applications:
+        if application.id == application_id:
+            applications.remove(application)
+            return 
+
+    raise HTTPException(
+        status_code = 404,
+        detail= "msg: application not found"
+        )
+
